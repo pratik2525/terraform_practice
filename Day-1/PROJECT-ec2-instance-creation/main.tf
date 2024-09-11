40b8bc2b02868bbac99bd3ceeafe96c8bf33cdb5
Pratik <pratik.fleming@gmail.com>
2024-09-11 19:47:16 -0400

    Changed AMI and Region

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+    region = "ca-central-1"  # Set your desired AWS region
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+    ami           = "ami-0c6d358ee9e264ff1"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
 }
